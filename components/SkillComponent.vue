ebc69649575abd31218678e08399b9f8bff37c54
Sudhanshu Siddh <ssiddh@iu.edu>
2018-03-23 23:44:46 -0400

    Skills page completed with server calls

---
+++ components/SkillComponent.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-container grid-list-xs text-xs-center>
+    <v-layout row wrap align-center>
+      <v-flex style="margin-top: 10px; width: 30px;" class="text-xs-center headline">
+          <v-avatar tile size="90">
+            <img class="logo" :src="skill.logo"/>
+        </v-avatar>
+        <p>
+            {{skill.skillName}}
+        </p>
+      </v-flex>
+      <v-flex>
+        <v-progress-circular :size="80" :width="15" :rotate="-90" :value="value" :class="skill.color">
+          {{value}}
+        </v-progress-circular>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+  export default {
+    props: ['skill'],
+    data() {
+      return {
+        interval: {},
+        value: 0
+      }
+    },
+    beforeDestroy() {
+      clearInterval(this.interval)
+    },
+    mounted() {
+      this.interval = setInterval(() => {
+        if (this.value >= this.skill.value) {
+          return
+        }
+        this.value += 5
+      }, 400)
+    }
+  }
+
+</script>
+<style scoped>
+  .android {
+    color: #A4C639;
+  }
+
+  .java {
+    color: #F89820;
+  }
+
+  .nativescript {
+    color: #2d46ff;
+  }
+
+  .vuejs {
+    color: #42b883;
+  }
+
+  .javascript {
+    color: #f7df1e;
+  }
+
+  .typescript {
+    color: #007ACC;
+  }
+
+  .coldfusion {
+    color: #000e27;
+  }
+
+  .html5 {
+    color: #e34f26;
+  }
+
+  .css3 {
+    color: #3B5998;
+  }
+
+  .xml {
+    color: black;
+  }
+
+  .sqlserver {
+    color: #ae0018;
+  }
+
+  .mysql {
+    color: #00758f;
+  }
+
+  .mobileapp {
+    color: #26A69A;
+  }
+
+  .softdesign {
+    color: #42A5F5;
+  }
+
+  .designpattern {
+    color: #5C6BC0;
+  }
+
+  .firebase {
+      color: #F57C00;
+  }
+
+  .logo {
+      width: auto;
+      height: 60px;
+  }
+
+</style>
--- pages/skills.vue
+++ pages/skills.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <iframe allowtransparency="true" frameborder="0" scrolling="no" seamless="seamless" src="http://colmdoyle.github.io/gh-activity/gh-activity.html?user=sudhanshu-15&type=user"
+  <div class="container-background">
+    <app-skill-component v-for="skill in skillDetails" :key="skill.id" :skill="skill"></app-skill-component>
+     <iframe allowtransparency="true" frameborder="0" scrolling="no" seamless="seamless" src="http://colmdoyle.github.io/gh-activity/gh-activity.html?user=sudhanshu-15&type=user"
       width="300" height="300"></iframe>
-    <h1>Skills Section</h1>
     <back-to-top bottom="50px" right="50px" visibleOffset="200">
       <span class="grey darken-3" style="display: flex; opacity:0.6;">
         <v-icon x-large color="white">keyboard_arrow_up</v-icon>
@@ -13,15 +13,33 @@
 
 <script>
   import BackToTop from 'vue-backtotop';
+  import SkillComponent from '@/components/SkillComponent.vue'
+  import axios from 'axios'
   export default {
     components: {
-      BackToTop
+      BackToTop,
+      'appSkillComponent': SkillComponent
+    },
+    asyncData() {
+      return axios.get('/skills.json')
+        .then((res) => {
+          return {
+            skillDetails: res.data.skillDetails
+          }
+        })
+        .catch((e) => {
+          error({
+            statusCode: 404,
+            message: 'Post not found'
+          });
+        })
     }
   }
-
 </script>
 
 <style scoped>
-
-
+.container-background {
+    background-image: url('https://firebasestorage.googleapis.com/v0/b/personal-website-e3515.appspot.com/o/gplaypattern_big.png?alt=media&token=da7cb917-3ccc-4f3a-8dc6-136dcbb23bfb');
+    background-repeat: repeat;
+}
 </style>
