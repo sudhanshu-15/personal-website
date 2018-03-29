7f29804ffd9b85d48447b48096508bd19d1e6049
Sudhanshu Siddh <ssiddh@iu.edu>
2018-03-28 22:16:11 -0400

    Added snackbar toh show error, error still remains in pdfjs

--- components/ResumeComponent.vue
+++ components/ResumeComponent.vue
@@ -4,18 +4,36 @@
         Download
         <v-icon right color="white">cloud_download</v-icon>
         </v-btn>
-        <pdf :src="url"></pdf>
+        <pdf :src="src" @error="errorLoading"></pdf>
+        <v-snackbar 
+          :timeout="timeout" 
+          :top="top" 
+          v-model="snackbar">
+          Unable to load pdf, please download instead!
+          <v-btn flat color="teal" @click.native="snackbar = false">Close</v-btn>
+        </v-snackbar>
     </div>
 </template>
 <script>
   import pdf from 'vue-pdf'
+  var loading = pdf.createLoadingTask('https://www.dl.dropboxusercontent.com/s/t5ev5wz80vio9ix/Sudhanshu_March12018.pdf');
   export default {
     components: {
       pdf
     },
     data () {
       return {
-        url: 'https://www.dl.dropboxusercontent.com/s/t5ev5wz80vio9ix/Sudhanshu_March12018.pdf'
+        src: loading,
+        url: 'https://www.dl.dropboxusercontent.com/s/t5ev5wz80vio9ix/Sudhanshu_March12018.pdf',
+        snackbar: false,
+        timeout: 6000,
+        top: true
+      }
+    },
+    methods: {
+      errorLoading () {
+        console.log("Error loading pdf");
+        this.snackbar = true;
       }
     }
   }
