ef64b95f3d52527fd7c3a547bab8333cfc369c08
Sudhanshu Siddh <ssiddh@iu.edu>
2018-03-29 00:36:42 -0400

    Added ignoreError on sentry, not able to fix pdfjs error

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Raven from 'raven-js'
 import RavenVue from 'raven-js/plugins/vue'
 
-Raven.config('https://a0b82c4ef3514d6cae50a8dab9b519b5@sentry.io/664442')
+Raven.config('https://a0b82c4ef3514d6cae50a8dab9b519b5@sentry.io/664442', {
+    ignoreErrors: ['externalLinkTarget']
+  })
   .addPlugin(RavenVue, Vue)
   .install()
