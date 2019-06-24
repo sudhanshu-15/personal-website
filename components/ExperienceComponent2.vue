e3f67fae9773e1ec0b9383e8289803c559544ef6
Sudhanshu Siddh <ssiddh@iu.edu>
2019-06-24 01:13:54 -0700

    Created new experence component using timeline component

---
+++ components/ExperienceComponent2.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-timeline>
+    <v-timeline-item
+      v-for="(experience, i) in experienceDetail"
+      :key="i"
+      color="red lighten-2"
+      small
+    >
+      <template v-slot:opposite>
+        <span class="headline font-weight-bold green--text" v-text="experience.dates"></span>
+      </template>
+      <v-flex>
+        <v-hover>
+          <v-card
+            color="orange lighten-1"
+            slot-scope="{ hover }"
+            :class="`elevation-${hover ? 12 : 2}`"
+            class="mx-auto"
+          >
+            <v-card-title class="headline">{{experience.job}}</v-card-title>
+            <v-card-text class="white text--primary">
+              <v-list>
+                <v-list-tile-content
+                  v-for="detail in experience.description"
+                  :key="detail.id"
+                >* {{detail.text}}</v-list-tile-content>
+              </v-list>
+              <v-item v-for="tech in experience.technology" :key="tech.id">
+                <v-chip small color="teal" text-color="white" disabled>
+                  <v-avatar>
+                    <v-icon>donut_small</v-icon>
+                  </v-avatar>
+                  {{tech.techName}}
+                </v-chip>
+              </v-item>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </v-flex>
+    </v-timeline-item>
+  </v-timeline>
+</template>
+
+<script>
+export default {
+  props: ["experienceDetail"]
+};
+</script>
--- pages/experience.vue
+++ pages/experience.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <section id="cd-timeline" class="cd-container">
-      <app-experience-component v-for="experience in experienceDetail" :key="experience.id" :experience="experience"></app-experience-component>
+      <app-experience-component :experienceDetail="experienceDetail"></app-experience-component>
     </section>
     <!-- cd-timeline -->
   </div>
 </template>
 
 <script>
-  import ExperienceComponent from '@/components/ExperienceComponent.vue'
+//import ExperienceComponent from '@/components/ExperienceComponent.vue'
+import ExperienceComponent from '@/components/ExperienceComponent2.vue'
 import axios from 'axios'
 export default {
     components: {
@@ -42,59 +43,4 @@ export default {
 </script>
 
 <style scoped>
-  html,
-  body {
-    background-color: #222c32;
-    height: 100%;
-    font-family: 'Open Sans', sans-serif;
-    box-sizing: border-box;
-  }
-
-  .cd-container {
-    width: 90%;
-    max-width: 1080px;
-    margin: 0 auto;
-    background: #FAFAFA;
-    padding: 0 10%;
-    border-radius: 2px;
-  }
-
-  .cd-container::after {
-    content: '';
-    display: table;
-    clear: both;
-  }
-
-  /* -------------------------------- 
- Main components 
- -------------------------------- */
-
-  #cd-timeline {
-    position: relative;
-    padding: 2em 0;
-    margin-top: 2em;
-    margin-bottom: 2em;
-  }
-
-  #cd-timeline::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 25px;
-    height: 100%;
-    width: 4px;
-    background: #CFD8DC;
-  }
-
-  @media only screen and (min-width: 1170px) {
-    #cd-timeline {
-      margin-top: 3em;
-      margin-bottom: 3em;
-    }
-    #cd-timeline::before {
-      left: 50%;
-      margin-left: -2px;
-    }
-  }
-
 </style>
